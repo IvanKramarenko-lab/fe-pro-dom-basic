8a448b24bcc1417711b35e61ee86e528c5b321fb
santakratos <santakratos10@gmail.com>
2022-07-09 00:37:45 +0300

    v1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const redCart = document.querySelectorAll('.item:nth-child(odd)');
+  redCart.forEach((item) => item.style.backgroundColor = "red");
+  // for (let i = 0; i < cart.length; i++) {
+  //   cart[i].style.backgroundColor = "red";
+  // }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  let blueCart = document.querySelector('li');
+  while (blueCart) {
+    if (blueCart.matches('.likedItem')) {
+      blueCart.style.backgroundColor = "blue";
+      break;
+    } else {
+      blueCart = blueCart.nextElementSibling;
+    }
+  }
+};
